892223cfdd167f24cadf5a4c1aabb9e4cadcc4fe
zumbojo <zumbojo@gmail.com>
2013-01-07 20:18:54 -0600

    Added ToneGenerator files.

---
+++ Classes/ToneGenerator.h
@@ -0,0 +1,13 @@
+//
+//  ToneGenerator.h
+//  ToneGenerator
+//
+//  Created by user on 1/7/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToneGenerator : NSObject
+
+@end
---
+++ Classes/ToneGenerator.m
@@ -0,0 +1,13 @@
+//
+//  ToneGenerator.m
+//  ToneGenerator
+//
+//  Created by user on 1/7/13.
+//
+//
+
+#import "ToneGenerator.h"
+
+@implementation ToneGenerator
+
+@end
