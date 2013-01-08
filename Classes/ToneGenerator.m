ad671feebd4afcad78c41d5cfe01877cfd8715ec
zumbojo <zumbojo@gmail.com>
2013-01-07 20:42:43 -0600

    Added notice.

--- Classes/ToneGenerator.m
+++ Classes/ToneGenerator.m
@@ -2,9 +2,21 @@
 //  ToneGenerator.m
 //  ToneGenerator
 //
-//  Created by user on 1/7/13.
+//  Created by Kevin Lawson on 2013-01-07.
+//  This is a wrapper for Matt Gallagher's fantastic tone generating code
+//  available at http://www.cocoawithlove.com/2010/10/ios-tone-generator-introduction-to.html
 //
+//  Original notice included below:
+//  ===============================
 //
+//  Created by Matt Gallagher on 2010/10/20.
+//  Copyright 2010 Matt Gallagher. All rights reserved.
+//
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
 
 #import "ToneGenerator.h"
 
